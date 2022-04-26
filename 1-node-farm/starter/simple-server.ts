13bd4e397698e730b70d0f724694ea5516ffbc27
Luzer99 <86791808+Luzer99@users.noreply.github.com>
2022-04-26 12:59:17 +0200

    update

--- 1-node-farm/starter/simple-server.ts
+++ 1-node-farm/starter/simple-server.ts
@@ -1,11 +1,30 @@
-import * as http from "http";
+import * as http from 'http';
+import { readFileSync } from 'fs';
+
+const data = readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
-    res.end("Hello from the server!");
+    const pathName = req.url;
+
+    if (['/', '/overview'].some((val) => val === pathName)) {
+      res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+      res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(data);
+    } else {
+      res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world',
+      });
+      res.end('<h1>Page not found!</h1>');
+    }
   }
 );
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listen to requests on http://localhost:8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listen to requests on http://localhost:8000');
 });
