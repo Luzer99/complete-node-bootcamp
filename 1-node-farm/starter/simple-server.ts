2800301ea6513b6cc735069623b0ef4e0a8fb5d3
Luzer <munir9902@gmail.com>
2022-05-05 10:56:27 +0200

    Added types and included template to backend.

--- 1-node-farm/starter/simple-server.ts
+++ 1-node-farm/starter/simple-server.ts
@@ -1,17 +1,45 @@
 import * as http from 'http';
 import { readFileSync } from 'fs';
+import { DataElement } from './types/data-element';
+
+const replaceTemplate = (temp: string, product: DataElement) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+}
+
+const tempOverview = readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const productData = JSON.parse(data);
+const dataObj: DataElement[] = JSON.parse(data);
 
 const server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
     const pathName = req.url;
 
+    //OVERVIEW PAGE
     if (['/', '/overview'].some((val) => val === pathName)) {
-      res.end('This is the OVERVIEW');
+      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(output);
+      //PRODUCT PAGE
     } else if (pathName === '/product') {
       res.end('This is the PRODUCT');
+    //API
     } else if (pathName === '/api') {
       res.writeHead(200, { 'Content-type': 'application/json' });
       res.end(data);
---
+++ 1-node-farm/starter/types/data-element.ts
@@ -0,0 +1,11 @@
+export interface DataElement {
+  id: string;
+  productName: string;
+  image: string;
+  from: string;
+  nutrients: string;
+  quantity: string;
+  price: string;
+  organic: boolean;
+  description: string;
+}
