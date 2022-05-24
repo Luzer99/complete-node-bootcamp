d9c3303aee805bafa1c9769ea0a9091ba98b6e68
Luzer99 <86791808+Luzer99@users.noreply.github.com>
2022-05-24 19:27:49 +0200

    Add first commit in 4 etap

---
+++ 4-natours/starter/app.ts
@@ -0,0 +1,122 @@
+import * as express from 'express';
+import { readFileSync, writeFile } from 'fs';
+
+const app = express();
+
+interface TourEntity {
+  id: number;
+  name: string;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  startDates: string[];
+}
+
+const tours: TourEntity[] = JSON.parse(
+  readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+const getAllTours = (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req: express.Request, res: express.Response) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req: express.Request, res: express.Response) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour: TourEntity = {
+    newId,
+    ...req.body,
+  };
+
+  tours.push(newTour);
+  writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req: express.Request, res: express.Response) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+const deleteTour = (req: express.Request, res: express.Response) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Server is listening on: http://localhost:3000');
+});
